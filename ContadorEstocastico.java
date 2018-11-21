309ab3898db8d366fd7c53d2de606c81f736b6f1
jtsimoes <jtsimoes10@outlook.com>
2018-11-21 11:51:24 +0000

    Improved 'ContadorEstocastico.java'

--- ContadorEstocastico.java
+++ ContadorEstocastico.java
@@ -1,6 +1,7 @@
 public class ContadorEstocastico{
  	private static double probAdd = 1;             // 2^-0 = 1, probabilidade inicial
-	private static int n = 0;					// Elementos
+	private static int n1 = 0;					// Elementos da primeira solucao
+	private static int n2 = 0;					// Elementos da segunda solucao
 
 	public ContadorEstocastico(){
 		primeiraSolucao();				// Caso nada seja especificado no construtor, executa o contador pela primeira forma
@@ -11,28 +12,39 @@ public class ContadorEstocastico{
 	}
 	
 	public void primeiraSolucao(){
-		if(probAdd >= Math.random()) {
-			n++;
-			probAdd = 1/(Math.pow(2,n)); // 2^-n
+		if (Math.random() < 0.5) {
+			n1++;
 		}
-		System.out.println(n);
-		System.out.println(probAdd);
+	}
+
+	public static int getN1() {
+		return n1;
+	}
+
+	public static double getProbAdd() {
+		return probAdd;
 	}
 
 	public void segundaSolucao(){
-		if (Math.random() < 0.5) {
-			n++;
-			probAdd = 1/(Math.pow(2,n)); // 2^-n
+		if(probAdd >= Math.random()) {
+			n2++;
+			probAdd = 1/(Math.pow(2,n2)); // 2^-n
 		}
-		System.out.println(n);
-		System.out.println(probAdd);
+	}
+
+	public static int getN2() {
+		return n2;
+	}
+
+	@Override
+	public String toString() {
+		return "Contador 1: " + n1 + "; Contador 2: " + n2;
 	}
 
 	// ##########################################################
 	// For test purposes only
 	public static void main(String[] args) {
-		@SuppressWarnings
-		// Acho que precisamos de indicar o numero de elementos ou então fazer um 'for' ou um 'while', para evitar andarmos a criar tantos construtores
+		// Fazer um 'for' ou um 'while' para evitar andarmos a criar tantos construtores
 		ContadorEstocastico one = new ContadorEstocastico();
 		ContadorEstocastico two = new ContadorEstocastico();
 		ContadorEstocastico three = new ContadorEstocastico();
@@ -40,6 +52,7 @@ public class ContadorEstocastico{
 		ContadorEstocastico otherWay = new ContadorEstocastico(1);
 		ContadorEstocastico otherWay2 = new ContadorEstocastico(1);
 		ContadorEstocastico otherWay3 = new ContadorEstocastico(1);
+		ContadorEstocastico otherWay4 = new ContadorEstocastico(1);
 	}
 	// ##########################################################
 }
