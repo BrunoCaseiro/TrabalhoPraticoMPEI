c6b3ef180d4273e239722494396d1e30c034cbc0
Bruno Caseiro <caseiro@ua.pt>
2018-11-24 16:51:30 +0000

    Getters, methods default, methods por preencher

--- CountingBloomFilter.java
+++ CountingBloomFilter.java
@@ -1,11 +1,39 @@
-public class CountingBloomFilter{
+public class CountingBloomFilter {
     private int n;                  //buckets aka tamanho do bloom filter
     private int k;                  //nº hash functions, arg do method insert()
     private int m;                  //elementos
     private int[] B;                //Bloom filter array
 
-    //***Getters***
-    public int getN() {
+    //***Methods gerados pelo VS Code***
+    @Override
+    public String toString() {
+        return "{" +
+            " n='" + getN() + "'" +
+            ", k='" + getK() + "'" +
+            ", m='" + getM() + "'" +
+            ", B='" + getB() + "'" +
+            "}";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof CountingBloomFilter)) {
+            return false;
+        }
+        CountingBloomFilter countingBloomFilter = (CountingBloomFilter) o;
+        return n == countingBloomFilter.n && k == countingBloomFilter.k && m == countingBloomFilter.m && Objects.equals(B, countingBloomFilter.B);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(n, k, m, B);
+    }
+
+    //End Methods do VS Code
+    // ***Getters***
+    public int size() { // ou getN()
         return n;
     }
 
@@ -20,24 +48,30 @@ public class CountingBloomFilter{
     public int[] getB() {
         return B;
     }
-    //*************
+    //End Getters
 
-    public CountingBloomFilter(int n, int k){
+    public CountingBloomFilter(int n, int k){           //Constructor
         this.m = 0;
         this.k = k;
     }
 
-    public void Initialization(){
+    public void initialize(){
         B = new int[n];                 //B é automaticamente initialized com todos os elemtnos a 0
     }
 
-    public void MembershipTest(){
+    public boolean isMember(){             //Membership Test
         
     }
 
-    public void insert(){
+    public void insert(int key){
 
     }
+
+    public void clear(){           
+
+    }
+
+
 
 
 
