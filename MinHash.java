70549a7579db5103730ffb84faed5d5f1715a9ac
Bruno Caseiro <caseiro@ua.pt>
2018-12-02 22:17:15 +0000

    added shingling

---
+++ MinHash.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+public class MinHash{
+    private int k;                               // k-shingle
+
+    public MinHash(int k){
+        this.k = k;
+    }
+
+    public int getK(){
+        return k;
+    }
+
+    public ArrayList<String> shingle(String s) {
+        int ss = k;              // ss = shingleSize
+        ArrayList<String> shingles = new ArrayList<String>();
+        boolean end = false;
+
+        while(!end){
+            if (ss >= s.length()){
+                end = true;
+                shingles.add(s.substring(ss-k, s.length()));
+            }
+            else{
+                shingles.add(s.substring(ss - k, ss));
+            }
+
+            ss += 1;                                      
+        }
+        return shingles;
+    }
+
+    // ##########################################################
+    // For test purposes only
+
+    public static void main(String[] args) {
+        MinHash x2 = new MinHash(2);
+        MinHash x3 = new MinHash(3);
+        MinHash x4 = new MinHash(4);
+        System.out.println(x2.shingle("abcd"));
+        System.out.println(x3.shingle("tomate"));
+        System.out.println(x4.shingle("alface Com Couves"));
+        System.out.println(x2.shingle("papaia"));
+        System.out.println(x3.shingle("rebentos de soja"));
+       
+        
+        
+        // RESULTADO ESPERADO:
+        // "ab", "bc", "cd"
+        // "tom", "oma", "mat", "ate"
+        // "alfa", "lfac", "face", "ace_", "ce_c", "e_co", "_com", "com_", "om_C", "m_Co", "_Cou", "Couv", ""ouve" "uves"
+        // "pa", "ap", "pa", "ai", "ia"
+        // "reb", "ebe", "ben", "ent", "nto", "tos", "os_", "s_d", "_de", "de_", "e_s", "_so", "soj", "oja"
+    
+    }
+    // ##########################################################
+}
+
