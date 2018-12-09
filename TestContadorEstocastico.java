a03789f70c84210f5ffaf555e8b41831d8758935
Bruno Caseiro <caseiro@ua.pt>
2018-12-09 20:11:10 +0000

    .

--- TestContadorEstocastico.java
+++ TestContadorEstocastico.java
@@ -3,7 +3,7 @@ public class TestContadorEstocastico {
         ContadorEstocastico one = new ContadorEstocastico();        // Inicia o contador pela primeira solucao
         ContadorEstocastico two = new ContadorEstocastico(1);       // Inicia o contador pela segunda solucao
 
-        System.out.println("Contagem com a primeira solucao:");
+        System.out.println("Contagem até 100 com a primeira solucao:");
         for (int i = 0; i < 100; i++) {		// 100 vezes
             one.primeiraSolucao();
             System.out.println(one.primeiraToString());
@@ -11,7 +11,7 @@ public class TestContadorEstocastico {
         System.out.println("Final result of the counter: "+ one.getN());
 
         System.out.println();
-        System.out.println("Contagem com a segunda solucao:");
+        System.out.println("Contagem até 100 com a segunda solucao:");
         for (int i = 0; i < 100; i++) { 	// 100 vezes
             two.segundaSolucao();
             System.out.println(two.segundaToString());
